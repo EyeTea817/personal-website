43c4c8a09ef5feabcdcefb5d07ebc33867e292b1
Ian <itemchin@gmail.com>
2025-01-18 00:11:09 -0500

    menu

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ import numpy as np
 
 st.set_page_config(page_title="Page Title", layout="wide")
 
-st.markdown("""
-    <style>
-        #MainMenu {visibility: hidden;}
-        #AppToolbar {visibility: hidden;}
-    </style>
-""", unsafe_allow_html=True)
+hide_streamlit_style = """
+            <style>
+                /* Hide the Streamlit header and menu */
+                header {visibility: hidden;}
+                /* Optionally, hide the footer */
+                .streamlit-footer {display: none;}
+                /* Hide your specific div class, replace class name with the one you identified */
+                .st-emotion-cache-uf99v8 {display: none;}
+            </style>
+            """
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 st.title('Ian Temchin')
